e09ee2f3ce000842027f6eb46adea5cc352959be
Aamir Khan <aamirksfg@gmail.com>
2025-07-25 12:03:54 -0700

    updating inference script

--- src/inference.py
+++ src/inference.py
@@ -6,16 +6,24 @@ import torch
 if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model_name = "NousResearch/Llama-2-7b-hf"
 
-    print(f"Loading model and tokenizer for {model_name}...")
-    model = load_model(model_name, quantized=False) 
-    tokenizer = load_tokenizer(model_name)
+    # Determine whether to use a fine-tuned model or the base model.
+    use_base = False             # Set this to 'True' if you want to use the base model.
+    use_local_finetuned = False  # Set this to 'True' if you want to load your locally fine-tuned model from the 'logs' directory.
 
+    model_path = "NousResearch/Llama-2-7b-hf" if use_base else "logs" if use_local_finetuned else "akhan365/llama2-finetuned-for-text2sql"
+
+    # Load the model and tokenizer.
+    print(f"\nLoading model and tokenizer from {model_path}...")
+    model = load_model(model_path, quantized=False) 
+    tokenizer = load_tokenizer(model_path)
+
+    # Play around with the prompt you want to generate text for.
     prompt = "The results are "
 
-    print(f"\nGenerating text for prompt: {prompt}")
-    print("=" * 50)
+    print(f'\nGenerating text using {'fine-tuned' if not use_base else 'base'} model...')
+    print('=' * 50)
+    print(f'Prompt: "{prompt}"\n')
     generated_text = generate(prompt, model, tokenizer, device)
-    print(f"\nGenerated text: {generated_text}\n")
+    print(f'\nGenerated text: "{generated_text}"\n')
 
